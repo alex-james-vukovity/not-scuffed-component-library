73d6581b0e54dac63bcd573d4fe8ebb993c6fcc7
alex-james-vukovity <alex.vukovity@gmail.com>
2021-03-20 10:59:33 +0000

    remove

--- src/stories/box/box.tsx
+++ src/stories/box/box.tsx
@@ -1,6 +1,3 @@
 import styled from 'styled-components/macro'
 
-export const Box = styled.div`
-  margin: ${({ theme }) => theme.space[0]};
-  padding: ${({ theme }) => theme.space[0]};
-`
+export const Box = styled.div``
